344e0f13bd7bbfa9cc4bb18638b21d6ec38f81ea
dariopalmesi <dariopalmesi88@gmail.com>
2024-11-13 16:40:10 +0100

    finish commit

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -2,10 +2,13 @@ import posts from '../data/Posts.js'
 import CardBlog from './CardBlog/CardBlog'
 
 export default function AppMain() {
+    const published_true = posts.filter(post => post.published === true)
+
+
     return (
         <main>
             <div className="container">
-                {posts.map(post => <CardBlog key={post.id} post={post} />)}
+                {published_true.map(post => <CardBlog key={post.id} post={post} />)}
             </div>
         </main>
     )
